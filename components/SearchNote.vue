ac241c6a63360a215f653239878c053d3849b7eb
Pietro Stivanello <68153217+pedrito-96@users.noreply.github.com>
2023-04-16 22:04:23 +0200

    not rounded

--- components/SearchNote.vue
+++ components/SearchNote.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center mt-2">
         <input v-model.trim="search" type="search" placeholder="Search..."
-        class="w-[50%] md:lg:w-[30%] border-2 rounded border-slate-300 focus:border-indigo-600 px-2">
+        class="w-[50%] md:lg:w-[30%] border-2 border-slate-300 focus:border-indigo-600 px-2">
     </div>
 </template>
 
